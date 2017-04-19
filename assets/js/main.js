ea203094d89dccfaf26753a96367a7a0e415a279
Arantza Burga <arybv18@gmail.com>
2017-04-18 19:05:38 -0500

    Ejercicio I - ESQUEMAS 02 - diseñando con javascrip 01

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+var contenedorP = document.getElementsByClassName("contenedorP")[0];
+var contenedorS = document.getElementsByClassName("contenedorS")[0];
+
+var naranja = document.getElementById("naranja");
+var morado = document.getElementById("morado");
+var amarillo = document.getElementById("amarillo");
+var azul = document.getElementById("azul");
+var negro = document.getElementById("negro");
+var verde = document.getElementById("verde");
+
+  contenedorP.style.position = "relative";
+  contenedorP.style.width = "100%";
+
+  naranja.style.background = "#ED5810";
+  naranja.style.width = "210px";
+  naranja.style.height = "210px";
+
+  morado.style.position = "absolute"
+  morado.style.background = "#8D3488";
+  morado.style.width = "140px";
+  morado.style.height = "140px";
+  morado.style.margin = "35px";
+
+  amarillo.style.position = "absolute"
+  amarillo.style.background = "#FFFC00";
+  amarillo.style.width = "70px";
+  amarillo.style.height = "70px";
+  amarillo.style.margin = "35px";
+
+  contenedorS.style.width = "100%";
+  contenedorS.style.width = "100%";
+
+  azul.style.background = "#2768EE";
+  azul.style.width = "210px";
+  azul.style.height = "210px";
+
+  negro.style.background = "#000000";
+  negro.style.width = "42px";
+  negro.style.height = "42px";
+  negro.style.margin = "35px";
+
+  verde.style.position = "absolute"
+  verde.style.background = "#469347";
+  verde.style.width = "70px";
+  verde.style.height = "70px";
+  verde.style.margin = "35px";
